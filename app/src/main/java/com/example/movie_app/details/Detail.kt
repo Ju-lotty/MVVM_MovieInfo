82a4d420776a232e3c1fa968e980a3889eb2d6ac
Ju-lotty <101933903+Ju-lotty@users.noreply.github.com>
2023-02-28 12:59:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/movie_app/details/Detail.kt
@@ -0,0 +1,12 @@
+package com.example.movie_app.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movie_app.R
+
+class Detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
